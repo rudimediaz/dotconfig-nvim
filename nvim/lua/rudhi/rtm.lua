6aa1d44fdb5206682f4871b4dfca5480750570ef
Rudhi Mediastara <42732580+rudimediaz@users.noreply.github.com>
2022-10-23 21:50:01 +0700

    follow up deprecation warning

--- nvim/lua/rudhi/rtm.lua
+++ nvim/lua/rudhi/rtm.lua
@@ -20,7 +20,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- cmp
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- autocompletion manual
 
